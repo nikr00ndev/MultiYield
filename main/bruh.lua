0a695e9f2c82e37e9a3b59edd929ffe150c0936d
nikr00ndev <ngiveros@gmail.com>
2024-07-21 23:26:38 +0700

    Update bruh.lua

--- main/bruh.lua
+++ main/bruh.lua
@@ -1,1 +1,51 @@
+local gui = Instance.new("ScreenGui")
+local a1 = Instance.new("ImageLabel")
+local a2 = Instance.new("ImageLabel")
+local a3 = Instance.new("ImageLabel")
+local a4 = Instance.new("ImageLabel")
+local a5 = Instance.new("ImageLabel")
+local a6 = Instance.new("ImageLabel")
+local a7 = Instance.new("ImageLabel")
+local a8 = Instance.new("ImageLabel")
+local a9 = Instance.new("ImageLabel")
 
+
+gui.Parent = game.Players.LocalPlayer.PlayerGui
+gui.Name = "MeatSpin"
+
+a1.Parent = gui
+a2.Parent = gui
+a3.Parent = gui
+a4.Parent = gui
+a5.Parent = gui
+a6.Parent = gui
+a7.Parent = gui
+a8.Parent = gui
+a9.Parent = gui
+a1.Position = UDim2.new(0, 0,0, 0)
+a2.Position = UDim2.new(0.077, 0,0, 0)
+a3.Position = UDim2.new(0.155, 0,0, 0)
+a4.Position = UDim2.new(0, 0,0.168, 0)
+a5.Position = UDim2.new(0.077, 0,0.168, 0)
+a6.Position = UDim2.new(0.155, 0,0.168, 0)
+a7.Position = UDim2.new(0, 0,0.335, 0)
+a8.Position = UDim2.new(0.077, 0,0.335, 0)
+a9.Position = UDim2.new(0.155, 0,0.335, 0)
+a1.Size = UDim2.new(0, 100, 0, 100)
+a2.Size = UDim2.new(0, 100, 0, 100)
+a3.Size = UDim2.new(0, 100, 0, 100)
+a4.Size = UDim2.new(0, 100, 0, 100)
+a5.Size = UDim2.new(0, 100, 0, 100)
+a6.Size = UDim2.new(0, 100, 0, 100)
+a7.Size = UDim2.new(0, 100, 0, 100)
+a8.Size = UDim2.new(0, 100, 0, 100)
+a9.Size = UDim2.new(0, 100, 0, 100)
+a1.Image = "http://www.roblox.com/asset/?id=18592714882"
+a2.Image = "http://www.roblox.com/asset/?id=18592720722"
+a3.Image = "http://www.roblox.com/asset/?id=18592724726"
+a4.Image = "http://www.roblox.com/asset/?id=18592729914"
+a5.Image = "http://www.roblox.com/asset/?id=18592734229"
+a6.Image = "http://www.roblox.com/asset/?id=18592740364"
+a7.Image = "http://www.roblox.com/asset/?id=18592744607"
+a8.Image = "http://www.roblox.com/asset/?id=18592748223"
+a9.Image = "http://www.roblox.com/asset/?id=18592752501"
