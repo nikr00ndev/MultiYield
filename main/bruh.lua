60f2dbc9cb47f31e2c073e0e290b990c8fe892d2
nikr00ndev <ngiveros@gmail.com>
2024-07-22 00:01:21 +0700

    Update bruh.lua

--- main/bruh.lua
+++ main/bruh.lua
@@ -12,6 +12,7 @@ local a9 = Instance.new("ImageLabel")
 
 gui.Parent = game.Players.LocalPlayer.PlayerGui
 gui.Name = "MeatSpin"
+gui.ResetOnSpawn = true
 
 a1.Parent = gui
 a2.Parent = gui
@@ -22,6 +23,15 @@ a6.Parent = gui
 a7.Parent = gui
 a8.Parent = gui
 a9.Parent = gui
+a1.Name = "1"
+a2.Name = "2"
+a3.Name = "3"
+a4.Name = "4"
+a5.Name = "5"
+a6.Name = "6"
+a7.Name = "7"
+a8.Name = "8"
+a9.Name = "9"
 a1.Position = UDim2.new(0, 0,0, 0)
 a2.Position = UDim2.new(0.077, 0,0, 0)
 a3.Position = UDim2.new(0.155, 0,0, 0)
@@ -31,20 +41,20 @@ a6.Position = UDim2.new(0.155, 0,0.168, 0)
 a7.Position = UDim2.new(0, 0,0.335, 0)
 a8.Position = UDim2.new(0.077, 0,0.335, 0)
 a9.Position = UDim2.new(0.155, 0,0.335, 0)
-a1.Size = UDim2.new(0, 100, 0, 100)
-a2.Size = UDim2.new(0, 100, 0, 100)
-a3.Size = UDim2.new(0, 100, 0, 100)
-a4.Size = UDim2.new(0, 100, 0, 100)
-a5.Size = UDim2.new(0, 100, 0, 100)
-a6.Size = UDim2.new(0, 100, 0, 100)
-a7.Size = UDim2.new(0, 100, 0, 100)
-a8.Size = UDim2.new(0, 100, 0, 100)
-a9.Size = UDim2.new(0, 100, 0, 100)
+a1.Size = UDim2.new(0.077, 0,0.168, 0)
+a2.Size = UDim2.new(0.077, 0,0.168, 0)
+a3.Size = UDim2.new(0.077, 0,0.168, 0)
+a4.Size = UDim2.new(0.077, 0,0.168, 0)
+a5.Size = UDim2.new(0.077, 0,0.168, 0)
+a6.Size = UDim2.new(0.077, 0,0.168, 0)
+a7.Size = UDim2.new(0.077, 0,0.168, 0)
+a8.Size = UDim2.new(0.077, 0,0.168, 0)
+a9.Size = UDim2.new(0.077, 0,0.168, 0)
 a1.Image = "http://www.roblox.com/asset/?id=18592714882"
 a2.Image = "http://www.roblox.com/asset/?id=18592720722"
 a3.Image = "http://www.roblox.com/asset/?id=18592724726"
 a4.Image = "http://www.roblox.com/asset/?id=18592729914"
-a5.Image = "http://www.roblox.com/asset/?id=18592734229"
+a5.Image = "http://www.roblox.com/asset/?id=18592761660"
 a6.Image = "http://www.roblox.com/asset/?id=18592740364"
 a7.Image = "http://www.roblox.com/asset/?id=18592744607"
 a8.Image = "http://www.roblox.com/asset/?id=18592748223"
