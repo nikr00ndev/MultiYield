2c534efd1fbd4ec02fbfd499563d04923ca2042e
nikr00ndev <ngiveros@gmail.com>
2024-07-22 12:14:22 +0700

    Create api.lua

---
+++ main/api/api.lua
@@ -0,0 +1,9 @@
+--MultiYield API
+
+local core = Instance.new("StringValue")
+core.Parent = game.ReplicatedStorage.MultiYield.Core
+
+
+if game.ReplicatedStorage.MultiYield.Key.Value == "BodyParty" then
+	game.ReplicatedStorage.MultiYield.Core.CoreAPI.Value = "iia72haf8ajqha7fyq81yqywr82726123d_dhahsqyagf_17dyhadsgafgq2"
+end
