747336df05c712768a5db1b89d4e7a938ee15567
nikr00ndev <ngiveros@gmail.com>
2024-07-22 12:10:40 +0700

    Update core.lua

--- core/core.lua
+++ core/core.lua
@@ -1,1 +1,17 @@
 --MultiYield Core
+local OkTextCore = Instance.new("StringValue")
+local OkTextMain = Instance.new("StringValue")
+wait(1)
+OkTextCore.Value = "false"
+OkTextMain.Value = "false"
+OkTextCore.Parent = game.ReplicatedStorage.MultiYield.Core
+OkTextMain.Parent = game.ReplicatedStorage.MultiYield.Main
+OkTextCore.Name = "CoresLoaded"
+OkTextMain.Name = "MainLoaded"
+
+
+wait(5)
+if game.ReplicatedStorage.MultiYield.Core.CoreLoader.Value == "true_V_2" and game.ReplicatedStorage.MultiYield.Core.Core.Value == "AMS_B_@nd_C_B_V_4" and game.ReplicatedStorage.MultiYield.Core.CoreMain.Value == "AMS_B_@nd_V_2" then
+	OkTextCore.Value = "true"
+end
+OkTextMain.Value = "true"
