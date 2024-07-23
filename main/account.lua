97a58ee9ebb74d232249246301d6b176f727ebbe
nikr00ndev <ngiveros@gmail.com>
2024-07-24 00:10:36 +0700

    Delete main/account.lua

--- main/account.lua
+++
@@ -1,7 +0,0 @@
-local Accounts ={
-	Admin = "supersas228_lol";
-	Markprogood228 = "lol"
-}
-
-print(Accounts)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/nikr00ndev/MultiYield/main/main/main.lua'))()
