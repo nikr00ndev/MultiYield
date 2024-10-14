a6304db16a43f7f770eee41df394e085d2278648
nikr00ndev <ngiveros@gmail.com>
2024-10-14 16:29:59 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nikr00ndev/MultiYield/refs/heads/main/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nikr00ndev/MultiYield/refs/heads/main/main/keysystem.lua"))()
