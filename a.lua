f1737a3de513f6e90d24220e2cdabab7dd276662
nikr00ndev <ngiveros@gmail.com>
2024-11-04 21:35:16 +0700

    Update a.lua

--- a.lua
+++ a.lua
@@ -5,7 +5,7 @@ FIXMainFolder.Name = "MultiYield"
 FIXMainFolder.Parent = game.ReplicatedStorage
 local FIXCoreLoader = Instance.new("Folder")
 FIXCoreLoader.Name = "CoreLoader"
-FIXCoreLoader.Parent = FIXMainFolder.Parent
+FIXCoreLoader.Parent = game.ReplicatedStorage.MultiYield
 
 -------------------------------------------------------------------------------
 --Locals
