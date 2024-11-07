77f1242cc3fdf3c8ecbd3044fcf1b3e97649cfa3
nikr00ndev <ngiveros@gmail.com>
2024-11-07 23:41:23 +0700

    Update keysystem.lua

--- old/keysystem.lua
+++ old/keysystem.lua
@@ -197,7 +197,7 @@ end)
 CheckKey.MouseButton1Click:Connect(function()
 	if KeyEnter.Text == Key then
 		KeyI.Value = Key
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/nikr00ndev/MultiYield/main/main/main/main.lua'))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/nikr00ndev/MultiYield/refs/heads/main/old/main.lua"))()
 		game.Players.LocalPlayer.PlayerGui.MultiYieldKeySystem:Destroy()
 	else
 		KeyEnter.Text = "Invalid Key!"
